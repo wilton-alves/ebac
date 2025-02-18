ca91183d528722aabf07ed3a79ebd20ca4d72ef1
Wilton Alves <walvesdg@gmail.com>
2025-02-18 11:57:45 -0300

    Subindo estudos em estatistica

---
+++ Python/4_matematica_e_Estatistica/modelo_classificacao.py
@@ -0,0 +1,57 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+import joblib
+
+df = pd.read_csv('clientes-v3-preparado.csv')
+
+# Categorizar 'salario' acima e abaixo da mediana
+df['salario_categoria'] = (df['salario'] > df['salario'].median().astype(int)) # 1 = acima da mediana, 0 = abaixo ou igual a mediana
+
+X = df[['idade', 'anos_experiencia', 'nivel_educacao_cod', 'area_atuacao_cod']] # Preditor
+Y = df['salario_categoria'] # Prever
+
+# Dividir os dados: Treinamento e Teste
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+# Criar e treinar o modelo de Regressão Logística
+modelo_lr = LogisticRegression()
+modelo_lr.fit(X_train, Y_train)
+
+# Para efeitos de comparação, vamos criar também um modelo de Árvore de Decisão
+modelo_dt = DecisionTreeClassifier()
+modelo_dt.fit(X_train, Y_train)
+
+# Prever valores de teste
+Y_prev_lr = modelo_lr.predict(X_test)
+Y_prev_dt = modelo_dt.predict(X_test)
+
+# Métricas de avaliação - Regressão Logística
+accuracy_lr = accuracy_score(Y_test, Y_prev_lr)
+precision_lr = precision_score(Y_test, Y_prev_lr)
+recall_lr = recall_score(Y_test, Y_prev_lr)
+
+print('\nMétricas de avaliação - Regressão Logística:')
+print(f'Acurácia (Regressão Logística): {accuracy_lr:.2f}')
+print(f'Precisão (Regressão Logística): {precision_lr:.2f}')
+print(f'Recall OU Sensibilidade (Regressão Logística): {recall_lr:.2f}')
+
+# Métricas de avaliação - Árvore de Decisão
+accuracy_dt = accuracy_score(Y_test, Y_prev_dt)
+precision_dt = precision_score(Y_test, Y_prev_dt)
+recall_dt = recall_score(Y_test, Y_prev_dt)
+
+print('\nMétricas de avaliação - Árvore de Decisão:')
+print(f'Acurácia (Árvore de Decisão): {accuracy_dt:.2f}')
+print(f'Precisão (Árvore de Decisão): {precision_dt:.2f}')
+print(f'Recall OU Sensibilidade (Árvore de Decisão): {recall_dt:.2f}')
+
+'''
+Para este conjunto de dados, o modelo de Regressão Logística obteve melhores resultados em termos de acurácia, precisão e recall.
+'''
+
+# Salvar os modelos treinados
+joblib.dump(modelo_lr, 'modelo_regressao_logistica.pkl')
+joblib.dump(modelo_dt, 'modelo_arvore_decisao.pkl')
---
+++ Python/4_matematica_e_Estatistica/modelo_regressao.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import root_mean_squared_error, r2_score
+import joblib
+
+df = pd.read_csv('clientes-v3-preparado.csv')
+
+# X = df[['anos_experiencia']] # Preditor
+X = df[['idade', 'anos_experiencia', 'nivel_educacao_cod', 'area_atuacao_cod']] # preditor
+
+Y = df['salario'] # prever
+
+
+# Dividir os dados: Treinamento e Teste
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+# Criar e treinar o modelo de Regressão Linear
+modelo_lr  = LinearRegression()
+modelo_lr.fit(X_train, Y_train)
+
+
+# Prever valores de teste
+Y_prev = modelo_lr.predict(X_test)
+
+
+# Métricas de avaliação
+r2 = r2_score(Y_test, Y_prev)
+print(f'Coeficiente de determinação (R²): {r2:.2f}')
+
+rmse = root_mean_squared_error(Y_test, Y_prev)
+print(f'Raiz do Erro Médio Quadrático (RMSE): {rmse:.2f}')
+print(f'Desvio Padrão do campo salario: {df["salario"].std():.2f}')
+
+'''
+Para X = df[['anos_experiencia']] a saída está muito próxima do Desvio Padrão do campo salario
+Isso significa que o modelo de regressão linear não consegue explicar muito bem a variabilidade nos dados.
+Note ainda que o R² está muito baixo. Ou seja, possui uma margem de erro muito alta.
+
+--- Saída para X = df[['anos_experiencia']] ---
+Coeficiente de determinação (R²): 0.18
+Raiz do Erro Médio Quadrático (RMSE): 3237.46
+Desvio Padrão do campo salario: 3478.14
+---
+
+Adicionando mais variáveis no modelo, otemos um resultado melhor.
+Para X = df[['idade', 'anos_experiencia', 'nivel_educacao_cod', 'area_atuacao_cod']]
+o R² teve um aumento significativo, indicando maior assertividade do modelo e o RMSE
+variou significativamente em relação ao desvio padrão do campo salario.
+
+--- Saída para X = df[['idade', 'anos_experiencia', 'nivel_educacao_cod', 'area_atuacao_cod']] ---
+Coeficiente de determinação (R²): 0.57
+Raiz do Erro Médio Quadrático (RMSE): 2334.96
+Desvio Padrão do campo salario: 3478.14
+---
+
+'''
+
+# Salvar o modelo treinado
+joblib.dump(modelo_lr, 'modelo_regressao_linear.pkl')
